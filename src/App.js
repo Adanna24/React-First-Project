7e2d32b4371536736b7cea7aa30ee93a15747b4a
Adanna24 <oziokoblessing397@gmail.com>
2022-06-12 07:34:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useEffect, useState } from "react";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Note from "./components/Notes/Note";
+import Todocontainer from "./components/Todo/Todocontainer";
+import Header from "./Header";
+import Meme from "./components/Meme-maker/meme";
+import Home from "./Home";
 
-function App() {
+function App () {
+
+  const [theme, settheme] = useState( () => JSON.parse(localStorage.getItem("theme-settings"))|| false)
+
+  useEffect(() => {
+    localStorage.setItem("theme-settings", JSON.stringify(theme))
+  }, [theme])
+
+  const changetheme = () => {
+    settheme(pretheme => !pretheme)
+  }
+
+  const [showhome, setshowhome] = useState(false)
+
+  const homedisplay = {
+    display: showhome? "block":"none"
+  }
+
+  const handleShow = () => {
+    setshowhome(showhome => !showhome)
+  }
+
+  const [searchtext, setsearchtext] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+          <Home 
+            key={Date.now}
+            show={showhome}
+            setshowhome={setshowhome}
+            handleshow={handleShow}
+          />
+          <main className={`trans ${theme? "darkmode":"" }`} style={homedisplay}>
+            <Header 
+              key={theme}
+              theme={theme}
+              changetheme={changetheme}
+              handleshow={handleShow}
+              search={searchtext}
+              setsearch={setsearchtext}
+            />
+            <Meme search={searchtext} />
+            <Note 
+                theme={theme}
+                changetheme={changetheme}
+                search={searchtext}
+            />
+              <Todocontainer search={searchtext} />
+          </main>
+     </div>     
+  )
 }
 
 export default App;
